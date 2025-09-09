8efb8607d2f4693625afec0e3d739ffd2d297c54
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2025-09-09 12:14:02 +0600

    Some Changes From Apon

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,37 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  
+
+    <title>{{ !empty($setting) ? $setting->site_name : 'Title' }} -
+        {{ !empty($setting) ? $setting->site_description : 'Slogan' }}
+    </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+
+    <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="{{ asset('assets/css/core/libs.min.css') }}" />
+
+    <!-- Hope Ui Design System Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/hope-ui.min.css?v=5.0.0') }}" />
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.min.css?v=5.0.0') }}" />
+
+    <!-- Customizer Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/customizer.min.css?v=5.0.0') }}" />
+
+    <!-- RTL Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/rtl.min.css?v=5.0.0') }}" />
+    <link rel="stylesheet" href="{{ asset('fonts/iconmind.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/default.css') }}">
+
+
+</head>
