6a122db9591f8c197ce7f4f77e3b035a9a9f7d7f
Md Shahajahan Ali <msacse1@gmail.com>
2025-09-23 10:34:58 +0600

    Some Modified

--- database/migrations/2025_09_22_062712_create_payrolls_table.php
+++ database/migrations/2025_09_22_062712_create_payrolls_table.php
@@ -14,11 +14,35 @@ return new class extends Migration
     public function up()
     {
         Schema::create('payrolls', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('emp_id');
+            $table->integer('emp_status');
+            $table->string('dept_id')->nullable();
+            $table->string('desig_id')->nullable();
+            $table->date('salary_month')->nullable();
+            $table->integer('n_days')->nullable();
+            $table->integer('present')->nullable();
+            $table->integer('absent')->nullable();
+            $table->integer('leave')->nullable();
+            $table->integer('weekend')->nullable();
+            $table->integer('holiday')->nullable();
+
+            $table->decimal('pay_day', 6, 2)->nullable();
+            $table->decimal('b_salary', 10, 2)->nullable();
+            $table->decimal('g_salary', 10, 2)->nullable();
+            $table->decimal('pay_salary', 10, 2)->nullable();
+            $table->decimal('total_allow', 10, 2)->nullable();
+            $table->string('all_allows')->nullable();
+
+            $table->decimal('absent_deduct', 10, 2)->nullable();
+            $table->decimal('loan_deduct', 10, 2)->nullable();
+            $table->decimal('pf_deduct', 10, 2)->nullable();
+            $table->decimal('others_deduct', 10, 2)->nullable();
+            $table->decimal('total_deduct', 10, 2)->nullable();
+            $table->decimal('net_salary', 10, 2)->nullable();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
