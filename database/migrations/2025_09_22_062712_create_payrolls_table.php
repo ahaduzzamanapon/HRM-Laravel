d77e4f7e3a0328e9b633b291f9f323359d0502ae
Md Shahajahan Ali <msacse1@gmail.com>
2025-09-27 16:25:06 +0600

    Some Modified

--- database/migrations/2025_09_22_062712_create_payrolls_table.php
+++ database/migrations/2025_09_22_062712_create_payrolls_table.php
@@ -17,27 +17,45 @@ return new class extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->integer('branch_id');
-            $table->integer('emp_status');
+            $table->integer('emp_type');
             $table->string('dept_id')->nullable();
             $table->string('desig_id')->nullable();
+            $table->integer('emp_status');
             $table->date('salary_month')->nullable();
             $table->integer('n_days')->nullable();
             $table->integer('present')->nullable();
             $table->integer('absent')->nullable();
-            $table->integer('leave')->nullable();
+            $table->integer('leaves')->nullable();
             $table->integer('weekend')->nullable();
             $table->integer('holiday')->nullable();
 
             $table->decimal('pay_day', 6, 2)->nullable();
+            $table->integer('grade');
             $table->decimal('b_salary', 10, 2)->nullable();
             $table->decimal('g_salary', 10, 2)->nullable();
+
             $table->decimal('pay_salary', 10, 2)->nullable();
+
+            $table->decimal('h_rent', 10, 2)->nullable();
+            $table->decimal('m_allow', 10, 2)->nullable();
+            $table->decimal('special_allow', 10, 2)->nullable();
+            $table->decimal('child_allow', 10, 2)->nullable();
+            $table->decimal('trans_allow', 10, 2)->nullable();
+            $table->decimal('pf_allow_bank', 10, 2)->nullable();
+
             $table->decimal('total_allow', 10, 2)->nullable();
             $table->string('all_allows')->nullable();
 
+            $table->decimal('gross_salary', 10, 2)->nullable();
+
             $table->decimal('absent_deduct', 10, 2)->nullable();
-            $table->decimal('loan_deduct', 10, 2)->nullable();
             $table->decimal('pf_deduct', 10, 2)->nullable();
+            $table->decimal('tax_deduct', 10, 2)->nullable();
+            $table->decimal('bene_deduct', 10, 2)->nullable();
+            $table->decimal('auto_mobile_d', 10, 2)->nullable();
+            $table->decimal('h_loan_deduct', 10, 2)->nullable();
+            $table->decimal('p_loan_deduct', 10, 2)->nullable();
+            $table->decimal('stump_deduct', 10, 2)->nullable();
             $table->decimal('others_deduct', 10, 2)->nullable();
             $table->decimal('total_deduct', 10, 2)->nullable();
             $table->decimal('net_salary', 10, 2)->nullable();
