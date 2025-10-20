24db94451b6ba65900a9db9db2aa67a5fde8b3d2
Md Shahajahan Ali <msacse1@gmail.com>
2025-10-20 11:22:45 +0600

    Some Modified

--- database/seeders/AttMachineDataSeeder.php
+++ database/seeders/AttMachineDataSeeder.php
@@ -20,6 +20,35 @@ class AttMachineDataSeeder extends Seeder
      */
 
 
+    public function run_copy()
+    {
+        $attendanceService = app(AttendanceService::class);
+        $users = User::all();
+        $date1 = Carbon::createFromFormat('Y-m-d', '2025-08-08');
+        $date2 = Carbon::createFromFormat('Y-m-d', '2025-08-20');
+
+        while ($date1->lte($date2)) {
+            $date = $date1->copy();
+            $date1 = $date1->addDay();
+
+            foreach ($users as $user) {
+
+                // Simulate a check-in
+                AttMachineData::create([
+                    'punch_id' => $user->punch_id,
+                    'date_time' => $date->copy()->setTime(9, rand(0, 15), rand(0, 59)),
+                ]);
+
+                // Simulate a check-out
+                AttMachineData::create([
+                    'punch_id' => $user->punch_id,
+                    'date_time' => $date->copy()->setTime(17, rand(0, 15), rand(0, 59)),
+                ]);
+
+                // $attendanceService->attn_process($date->format('Y-m-d'),  $user->id);
+            }
+        }
+    }
     public function run()
     {
         $attendanceService = app(AttendanceService::class);
